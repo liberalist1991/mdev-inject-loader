6222262fdcf721680419260feced03cada86dac2
liberalist <zhan1991121@163.com>
2017-09-03 12:33:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+const loaderUtils = require('loader-utils')
+const path = require('path')
+const fs = require('fs')
+
+module.exports = function (source) {
+
+    if (this.cacheable) {
+        this.cacheable(true)
+    }
+    const callback = this.async();
+    const options = loaderUtils.getOptions(this);
+
+    // /src/module/module-1/module-1.js
+    const jsPath = this.resourcePath;
+
+    let result = wrapJs(source, jsPath);
+
+    callback(null, result);
+}
+
+function wrapJs (source, jsPath) {
+
+    // /src/module/module-1
+    const modulePath = path.dirname(jsPath),
+        moduleParent = path.dirname(modulePath),
+        moduleName = path.basename(modulePath);
+    // 只处理module目录下的
+    if ( moduleParent.indexOf('module') < 0) {
+        return source;
+    }
+
+    let selector = 'div[node-type~="module"].module-' + moduleName;
+
+    source = source.replace(/mdev\.dom\.build(\s\t)*\(/ig, ($) => {
+        return `mdev.dom.build('${selector}',`
+    })
+    source = source.replace(/mdev\.dom\.getInstance(\s\t)*\(/ig, ($) => {
+        return `mdev.dom.getInstance('${selector}',`
+    })
+    source = source.replace(/mdev\.message\.listen(\s\t)*\(/ig, ($) => {
+        return `mdev.message.listen('${moduleName}',`
+    })
+
+    // if (mdev && mdev.modules) {
+    //     mdev.modules.push(moduleName);
+    // }
+    return source
+};
